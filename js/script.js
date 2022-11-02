1939e7d1cfe443cc9f0efdb7aa147a76a0c8a98e
AndreaMatarrese <matarreseandrea47@gmail.com>
2022-11-02 12:09:20 +0100

    carousel without thumbs

--- js/script.js
+++ js/script.js
@@ -14,11 +14,47 @@ const bntPrev = document.querySelector('.prev')
 
 for (i=0; i < eleImages.length; i++){
     // creo l'immagine
-    const eleImg = document.createElement('img')
-    eleImg = eleImages[i];
-   
-    // aggiungo all'immagine una classe
-    eleImg.classList.add('slider-image')
+    const eleImg = document.createElement('img');
+    eleImg.src = eleImages[i];
+    eleImg.classList.add('slider-img');
 
-    if(i==0)
+
+if(i === 0){
+
+        eleImg.classList.add('active');
+    }  
+     eleSlider.append(eleImg);
 }
+// const listEleImg =  document.querySelectorAll('slider-image');
+const listEleImg = document.querySelectorAll('.slider-img');
+let activeIndex=0;
+
+
+// freccia destra
+btnNext.addEventListener('click', function(){
+    listEleImg[activeIndex].classList.remove('active');
+    
+
+    activeIndex++;
+
+    listEleImg[activeIndex].classList.add('active')
+    // bntPrev.classList.remove('hidden');
+    eleBtnLeft.classList.remove('hidden');
+	if (activeIndex === listEleImg.length - 1) {
+		eleBtnRight.classList.add('hidden');
+	}
+    
+});
+
+// freccia sinistra
+bntPrev.addEventListener('click', function(){
+    listEleImg[activeIndex].classList.remove('active');
+
+    activeIndex--;
+
+    listEleImg[activeIndex].classList.add('active')
+    eleBtnRight.classList.remove('hidden');
+	if (activeIndex === 0) {
+		eleBtnLeft.classList.add('hidden');
+	}
+})
